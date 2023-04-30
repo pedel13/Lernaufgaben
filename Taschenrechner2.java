be9cb6678a5dc4dec2c559b94b751a02c4bf57e2
pedel13 <wetpat13@mail.com>
2023-04-30 18:59:39 +0200

    fertig

--- Taschenrechner2.java
+++ Taschenrechner2.java
@@ -8,10 +8,35 @@ public class Taschenrechner2 {
         boolean menu = true;
 
         while (menu){
-            System.out.println("Geben sie ")
+            System.out.println("Geben sie ein: Addition; 1  Suptraktion; 2  Multiplikation; 3  Division; 4  Modulo; 5");
+            int rechnungsArt = sc.nextInt();
+            System.out.println("Geben sie die erste Zahl ein:");
+            int ersteZahl = sc.nextInt();
+            System.out.println("Geben sie die Zweite Zahl ein:");
+            int zweiteZahl = sc.nextInt();
+            switch (rechnungsArt){
+                case 1:
+                    System.out.println(ersteZahl + zweiteZahl);
+                    break;
+                case 2:
+                    System.out.println(ersteZahl - zweiteZahl);
+                    break;
+                case 3:
+                    System.out.println(ersteZahl * zweiteZahl);
+                    break;
+                case 4:
+                    System.out.println(ersteZahl / zweiteZahl);
+                    break;
+                case 5:
+                    System.out.println(ersteZahl % zweiteZahl);
+                    break;
+                default:
+                    System.out.println("Falsche Eingabe!");
+
+            }
 
         }
 
     }
-    
+
 }
--- VerUndEntschlusslung.java
+++ VerUndEntschlusslung.java
@@ -13,11 +13,20 @@ public class VerUndEntschlusslung {
             char auswahl = sc.next().charAt(0);
             
             if (auswahl == 'v'){
-                encrypt();
+                System.out.println("Gib dein Passwort ein:");
+                String passwort = sc.next();
+                System.out.println("Welche zahl wollen sie zum codiren:");
+                int codierNummer = sc.nextInt();
+                String neuPassword = encrypt(passwort, codierNummer);
+                System.out.println(neuPassword);
             }
             
             if (auswahl == 'e'){
-                edecrypt();
+                System.out.println("Geben sie ein verschlüsseltes Passwort ein?");
+                String passwort = sc.next();
+                System.out.println("Welchen Codierschlüssel wollen sie ferwenden");
+                int codierSchlussel = sc.nextInt();
+                System.out.println(edecrypt(passwort, codierSchlussel));
                 
             }
             
@@ -28,86 +37,70 @@ public class VerUndEntschlusslung {
     }
     
     
-    static void encrypt(){
+    static String encrypt(String passwort, int codierNummer){
         
 
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Gib dein Passwort ein:");
-        String passwort = sc.nextLine();
-        int textLange = passwort.length();
-        char[] neuPassword = new char[textLange];
-        int position = 0;
+        String neuPassword = "";
 
-
-        System.out.println("Welche zahl wollen sie zum codiren:");
-        int codierNummer = sc.nextInt();
-        
          for (char c : passwort.toCharArray()){
-            char buchstabe = c;
-            char klein = Character.toLowerCase(buchstabe);
-            if (klein >= 'a' & klein <= 'z'){
+            char klein = Character.toLowerCase(c);
+
+            if (klein >= 'a' && klein <= 'z'){
                 klein = (char)(klein + codierNummer);
                 int rest =  (int)'z' - klein; 
+
                     if (klein > 'z'){
                         klein = 'a';
                         klein = (char) (klein + rest);
                     }
                 
             }
-            if (klein <= '9' & klein >= '0'){
+            if (klein <= '9' && klein >= '0'){
+
                 for (int j = 0; j < codierNummer; j++){
                     klein++;
+                
                     if (klein > '9'){
                         klein = '0';
                     }
                 } 
             }
-            neuPassword[position] = klein;
-            position++;
+           neuPassword = neuPassword + klein;
         }
 
-        System.out.println(neuPassword);  
+        return neuPassword;  
     }
 
-    static void edecrypt() {
+    static String edecrypt(String passwort, int codierSchlussel) {
 
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Geben sie ein verschlüsseltes Passwort ein?");
-        String passwort = sc.nextLine();
-        int textLenge = passwort.length();
-        char[] neuPassword = new char[textLenge];
-        int position = 0;
+        String neuPassword = "";
 
-        System.out.println("Welchen Codierschlüssel wollen sie ferwenden");
-        int codierSchlussel = sc.nextInt();
 
         for (char c : passwort.toCharArray()){
-            char buchstabe = c;
-            char gross = Character.toUpperCase(buchstabe);
+            char gross = Character.toUpperCase(c);
 
-            if (gross <= 'Z' & gross >= 'A'){
+            if (gross <= 'Z' && gross >= 'A'){
                 gross = (char)(gross - codierSchlussel);
                 int rest = gross - (int)'A';
+
                     if (gross < 'A'){
                         gross = (char)(gross + rest);
                     }
             }
 
-            if (gross <= '9' & gross >= '0'){
+            if (gross <= '9' && gross >= '0'){
                 for (int j = 0; j < codierSchlussel;j++){
                     gross--;
                     if (gross < '0'){
+
                         gross = '9';
                     }
                 }
             }
 
-            neuPassword[position] = gross;
-            position++;
+            neuPassword = neuPassword + gross;
         }
-        
-        System.out.println(neuPassword);
+        return neuPassword; 
     }
 }
