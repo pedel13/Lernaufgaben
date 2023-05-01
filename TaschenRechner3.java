c4bebee1d3d1c5983317efbb6df17daa0f87cdfc
pedel13 <wetpat13@mail.com>
2023-05-01 18:20:16 +0200

    en dauerhafte

---
+++ TaschenRechner3.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class TaschenRechner3 {
+
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Geben sie eine zahl ein ");
+        int resultat = sc.nextInt();
+        boolean leuft = true;
+        char operator = 'a';
+        int zahl = 0;
+
+        while (leuft){
+            System.out.println("Wählen sie ein Operator: +, -, *, /, = für endresultat, c um zu löschen der rechnung, q um zu beehnden");
+            operator = sc.next().charAt(0);
+            switch (operator){
+                case '+':
+                    System.out.pritel("Geben sie noch eine Zahl ein:");
+                    zahl = sc.nextInt();
+                    resultat = resultat + zahl;
+                    break;
+                case '-':
+                    System.out.pritel("Geben sie noch eine Zahl ein:");
+                    zahl = sc.nextInt();
+                    resultat = resultat - zahl;
+                    break;
+                case '*':
+                    System.out.pritel("Geben sie noch eine Zahl ein:");
+                    zahl = sc.nextInt();
+                    resultat = resultat * zahl;
+                    break;
+                case '/':
+                    System.out.pritel("Geben sie noch eine Zahl ein:");
+                    zahl = sc.nextInt();
+                    resultat = resultat / zahl;
+                    break;
+                case '=':
+                    System.out.println(resultat);
+                    break;
+                case 'c':
+                    clear();
+                    System.out.println("Geben sie eine Zahl ein:");
+                    break;
+                case 'q':
+                    leuft = false;
+                    break;
+                default:
+                    System.out.println("falsche eingabe Bitte Widerholen");
+                    break;
+
+            }
+        }
+    }
+    
+}
